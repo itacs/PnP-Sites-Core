4bc710c612aed2236bdad7073b122916c20dcb55
Michael Klingner <michael.klingner@itacs.de>
2016-05-17 10:12:10 +0200

    sign Test & heal the Build

--- Core/OfficeDevPnP.Core/Properties/AssemblyInfo.cs
+++ Core/OfficeDevPnP.Core/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2.2.1603.0")]
 [assembly: AssemblyFileVersion("2.2.1603.0")]
 
-//[assembly: InternalsVisibleTo("OfficeDevPnP.Core.Tests")]
+[assembly: InternalsVisibleTo("OfficeDevPnP.Core.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010061b9f6b6d8ba217c01ffa04a0c820cfd663d4e5683e9ac6be2ca6938e89eb5bd0020cec79858caf59b0737eba727a0406ab1c6f1c655300b013dae49a775a4859b5269405e999fff40dd4f4ecd945e762464df77bc313bc1c028bb6346f77d0761cb960be9d7af1ba5ef7cc3aaed22dfc914b8b0ea487a188cb65dd3982459bb")]
